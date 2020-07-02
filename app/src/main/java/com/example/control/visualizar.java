71334d0534904f211d2f1e8c53dc454d839e2e67
WilliamMelo <williammelo@hotmail.com>
2020-07-02 11:38:08 -0500

    boton de salir agregado

--- app/src/main/java/com/example/control/visualizar.java
+++ app/src/main/java/com/example/control/visualizar.java
@@ -1,9 +1,11 @@
 package com.example.control;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class visualizar extends AppCompatActivity {
 TextView v1,v2,v3,v4;
@@ -21,14 +23,22 @@ TextView v1,v2,v3,v4;
         String d4=extras.getString("d4");
 
         v1=(TextView)findViewById(R.id.v1);
-        v1.setText("Nombre: "+d1);
-        v2=(TextView)findViewById(R.id.v2);
-        v2.setText("Cedula: "+d2);
-        v3=(TextView)findViewById(R.id.v3);
-        v3.setText("Fecha de Ingreso: "+d3);
-        v4=(TextView)findViewById(R.id.v4);
-        v4.setText("Temperatura: "+d4);
+        v1.setText("Nombre: " + d1);
+        v2 = (TextView) findViewById(R.id.v2);
+        v2.setText("Cedula: " + d2);
+        v3 = (TextView) findViewById(R.id.v3);
+        v3.setText("Fecha de Ingreso: " + d3);
+        v4 = (TextView) findViewById(R.id.v4);
+        v4.setText("Temperatura: " + d4);
 
 
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
     }
 }
